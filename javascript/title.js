70160ec12dc17b68077409ae9b81028cbccb198a
manimanearly <manimani23230139@yahoo.co.jp>
2020-09-13 23:19:10 +0900

    クリック時の処理が間違っていたため修正 #4 #9

--- javascript/title.js
+++ javascript/title.js
@@ -1,5 +1,5 @@
 var random_button = document.getElementById("random-match");
 
-random_button.addEventListener("click", function(){
-    fetch("../php/create_id.php")
-});
+function create_user(){
+    fetch("../php/create_id.php");
+}
