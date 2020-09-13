833e65128006e8e91d47c97d84af6dad0d0c8bdb
manimanearly <manimani23230139@yahoo.co.jp>
2020-09-13 23:32:12 +0900

    ユーザー作成のエラーを修正

--- php/create_id.php
+++ php/create_id.php
@@ -2,9 +2,9 @@
     require_once('common.php');
     $id = mt_rand(100000, 999999);
 
-    if(!getDB('insert into user(playerID) value(?)', $id)){
+    if(getDB('insert into user(playerID) value(?)', $id)[1] <= 0){
         $id = mt_rand(100000, 999999);
     }
 
-    
+
 ?>
